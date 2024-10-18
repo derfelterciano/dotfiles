fa2da0dc394b60fb58591230121ecf41f3aa31c8
SauceBoss1 <dtvsworld@gmail.com>
2024-10-18 14:24:18 -0700

    wezterm edits

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Ayu Mirage (Gogh)"
 
 config.font = wezterm.font("MesloLGS Nerd Font")
-
+config.font_size = 11
 -- config.enable_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
