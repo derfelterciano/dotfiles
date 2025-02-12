84db625f89239d2aaeb82338bf3aa2f01ee3b64b
SauceBoss1 <dtvsworld@gmail.com>
2025-02-12 15:53:03 -0800

    edit mods

--- .config/nvim/lua/plugins/nvim_tree.lua
+++ .config/nvim/lua/plugins/nvim_tree.lua
@@ -2,10 +2,47 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
-		require("nvim-tree").setup({
-			filters = {
-				dotfiles = true,
+		require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
+			renderer = {
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+						modified = true,
+						hidden = true,
+						diagnostics = true,
+						bookmarks = true,
+					},
+				},
 			},
-		})
+			diagnostics = {
+				enable = false,
+				show_on_dirs = false,
+				show_on_open_dirs = true,
+				debounce_delay = 500,
+				severity = {
+					min = vim.diagnostic.severity.HINT,
+					max = vim.diagnostic.severity.ERROR,
+				},
+				icons = {
+					hint = "",
+					info = "",
+					warning = "",
+					error = "",
+				},
+			},
+			filters = {
+				enable = false,
+				git_ignored = true,
+				dotfiles = false,
+				git_clean = false,
+				no_buffer = false,
+				no_bookmark = false,
+				custom = {},
+				exclude = {},
+			},
+		}) -- END_DEFAULT_OPTS)
 	end,
 }
