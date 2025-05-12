c59d40b67c684ebf6ad811cf1f562bd0747ab875
SauceBoss1 <dtvsworld@gmail.com>
2025-05-12 16:34:58 -0700

    tweaking blink

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -12,6 +12,8 @@ return {
 				require("luasnip.loaders.from_vscode").lazy_load()
 			end,
 		},
+		{ "nvim-tree/nvim-web-devicons" },
+		{ "onsails/lspkind.nvim" },
 	},
 
 	version = "1.*",
@@ -41,12 +43,53 @@ return {
 
 		snippets = { preset = "luasnip" },
 
-		signature = { enabled = true, trigger = { enabled = true } },
-
+		signature = {
+			enabled = true,
+			trigger = { enabled = true, show_on_insert = true },
+			window = { border = "single" },
+		},
 		completion = {
+			menu = {
+				border = "single",
+				draw = {
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local icon = ctx.kind_icon
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										icon = dev_icon
+									end
+								else
+									icon = require("lspkind").symbolic(ctx.kind, {
+										mode = "symbol",
+									})
+								end
+
+								return icon .. ctx.icon_gap
+							end,
+
+							-- Optionally, use the highlight groups from nvim-web-devicons
+							-- You can also add the same function for `kind.highlight` if you want to
+							-- keep the highlight groups in sync with the icons.
+							highlight = function(ctx)
+								local hl = ctx.kind_hl
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										hl = dev_hl
+									end
+								end
+								return hl
+							end,
+						},
+					},
+				},
+			},
 			list = { selection = { preselect = false } },
 			ghost_text = { enabled = true },
-			documentation = { auto_show = true },
+			documentation = { auto_show = true, auto_show_delay_ms = 100, window = { border = "single" } },
 		},
 
 		sources = {
