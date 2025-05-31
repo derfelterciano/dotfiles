78f4b438e2ffcfcf9c5c64e3ffc7927d51832839
SauceBoss1 <dtvsworld@gmail.com>
2025-05-31 11:53:52 -0700

    got codelldb working!

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -58,31 +58,50 @@ return {
 			name = "lldb",
 		}
 
+		-- c/c++/rust codelldb
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = vim.fn.expand("$MASON/bin/codelldb"),
+		}
+
 		-- lang config
+		-- dap.configurations.cpp = {
+		-- 	{
+		-- 		name = "Launch",
+		-- 		type = "lldb",
+		-- 		request = "launch",
+		-- 		program = function()
+		-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		-- 		end,
+		-- 		cwd = "${workspaceFolder}",
+		-- 		stopOnEntry = false,
+		-- 		args = {},
+		--
+		-- 		-- 💀
+		-- 		-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+		-- 		--
+		-- 		--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+		-- 		--
+		-- 		-- Otherwise you might get the following error:
+		-- 		--
+		-- 		--    Error on launch: Failed to attach to the target process
+		-- 		--
+		-- 		-- But you should be aware of the implications:
+		-- 		-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+		-- 		-- runInTerminal = false,
+		-- 	},
+		-- }
+
 		dap.configurations.cpp = {
 			{
-				name = "Launch",
-				type = "lldb",
+				name = "Launch file",
+				type = "codelldb",
 				request = "launch",
 				program = function()
 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 				end,
 				cwd = "${workspaceFolder}",
 				stopOnEntry = false,
-				args = {},
-
-				-- 💀
-				-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-				--
-				--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-				--
-				-- Otherwise you might get the following error:
-				--
-				--    Error on launch: Failed to attach to the target process
-				--
-				-- But you should be aware of the implications:
-				-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-				-- runInTerminal = false,
 			},
 		}
 	end,
